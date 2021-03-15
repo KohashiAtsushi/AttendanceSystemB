55462efa43c0bc70fe4d2856ef9331d96260bf40
AtsushiKohashi <melka.aquan@gmail.com>
2021-03-15 15:09:28 +0000

    Finish add-attendance-menu

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,10 +1,10 @@
+
 class AttendancesController < ApplicationController
+  before_action :set_user, only: [:edit_one_month, :update_one_month]
+  before_action :logged_in_user, only: [:update, :edit_one_month]
+  before_action :set_one_month, only: :edit_one_month
+  
   UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。"
-  before_action :set_user, only: [:index, :update]
-  before_action :set_one_month, only: :index
-  def index
-    
-  end
   
   def update
     @attendance = Attendance.find(params[:id])
@@ -25,35 +25,38 @@ class AttendancesController < ApplicationController
     redirect_to @user
   end
   
-  private
+  def edit_one_month
+  end
   
-    def user_params
-      params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
+  def update_one_month
+    ActiveRecord::Base.transaction do # トランザクションを開始します。
+      attendances_params.each do |id, item|
+        attendance = Attendance.find(id)
+        attendance.update_attributes!(item)
+      end
+    end
+    flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
+    redirect_to user_url(date: params[:date])
+  rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐です。
+    flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
+    redirect_to attendances_edit_one_month_user_url(date: params[:date])
+  end
+
+  private
+
+    # 1ヶ月分の勤怠情報を扱います。
+    def attendances_params
+      params.require(:user).permit(attendances: [:started_at, :finished_at, :note])[:attendances]
     end
 
     # beforeフィルター
 
-    # paramsハッシュからユーザーを取得します。
-    def set_user
-      @user = User.find(params[:user_id])
-    end
-
-    # ログイン済みのユーザーか確認します。
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "ログインしてください。"
-        redirect_to login_url
-      end
-    end
-
-    # アクセスしたユーザーが現在ログインしているユーザーか確認します。
-    def correct_user
-      redirect_to(root_url) unless current_user?(@user)
-    end
-
-    # システム管理権限所有かどうか判定します。
-    def admin_user
-      redirect_to root_url unless current_user.admin?
+    # 管理権限者、または現在ログインしているユーザーを許可します。
+    def admin_or_correct_user
+      @user = User.find(params[:user_id]) if @user.blank?
+      unless current_user?(@user) || current_user.admin?
+        flash[:danger] = "編集権限がありません。"
+        redirect_to(root_url)
+      end  
     end
 end
